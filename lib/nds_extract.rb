ac2b59e29da5cf82ada77cd3b75a666b04d51464
John Reiner <johnreiner1990@gmail.com>
2020-01-03 19:52:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,11 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  directors = []
+  directors_totals(source).each do |k, v|
+    directors.push(k)
+  end
+  return directors
 end
 
 def total_gross(source)
@@ -44,5 +48,5 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  return total 
+  return total
 end
